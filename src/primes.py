7894bf2b64147d6c8aba7d2696be11d72fffb66e
Vidant Kaul <vidantkaul@hotmail.com>
2025-07-16 11:54:26 -0700

    Added the function 'generate_len_primes' to primes.py

--- src/primes.py
+++ src/primes.py
@@ -11,8 +11,12 @@ def generate_primes(n: int):
     assert isinstance(n, int) and n > 1, f"Error: input ({n}) should be a int and > 1"
     return [x for x in range(2,n+1) if is_prime(x)]
 
+def generate_len_primes(n : int):
+    assert isinstance(n, int) and n > 1, f"Error: input ({n}) should be a int and > 1"
+    return len(generate_primes(n))
+
 if __name__ == '__main__':
 
     for N in range(10, 1000, 10):
-        L = generate_primes(N)
-        print(N, len(L))
+        L = generate_len_primes(N)
+        print(N, L)
