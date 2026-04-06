0961ecba0acda964d68d909d2949a3234ed1a7e9
Vidant Kaul <vidantkaul@hotmail.com>
2026-04-06 11:21:54 -0700

    The function_check function is in the demo

--- PulseScript Language/PulseScript.py
+++ PulseScript Language/PulseScript.py
@@ -3,7 +3,8 @@
 #   Make the function check function work
 
 special_chars = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "[", "{", "]", "}", "\\", 
-                 "|", ";", ":", ",", "<", ">", "/", "?"]  # removed "." to allow floats
+                 "|", ";", ":", ",", "<", ">", "/", "?"]  # removed "." to allow floats in variables and "=" 
+                                                          # to allow a variable be equal to something
 
 # Make sure there's nothing wrong with the syntax of a variable
 def var_check(variable: str, debug: bool = False) -> None:
@@ -50,44 +51,41 @@ def var_check(variable: str, debug: bool = False) -> None:
         # Value is a string, everything inside quotes is allowed
         pass
     else:
-        # Value should be a number (int or float with optional leading +/-)
-        dot_count = 0
-        for i, char in enumerate(value_str):
-            if char == ".":
-                dot_count += 1
-                if dot_count > 1:
-                    raise SyntaxError(f"{variable} has invalid float value with multiple dots")
-            elif char in "+-" and i == 0:
-                continue  # allow leading + or -
-            elif not char.isdigit():
-                raise SyntaxError(f"{variable} has invalid character '{char}' in value")
+        # Value should be a binary value or a number (int or float with optional leading +/-)
+        if value_str.startswith('0b'):
+            if len(value_str) <= 2: raise SyntaxError(f"{variable} has invalid binary code in it")
+            for b in value_str[2:]:
+                if b not in ["0", "1"]: raise SyntaxError(f"{variable} has invalid binary code in it")
+        else:
+            dot_count = 0
+            for i, char in enumerate(value_str):
+                if char == ".":
+                    dot_count += 1
+                    if dot_count > 1: raise SyntaxError(f"{variable} has invalid float value with multiple dots")
+                elif char in "+-" and i == 0: continue
+                elif not char.isdigit(): raise SyntaxError(f"{variable} has invalid character '{char}' in value")
 
     # Return detailed info for debugging
     if debug: return (f"variable: {variable}\nhas_equal: {has_equal}\nplace: {place}\n"
             f"name: {name}\nValue: {Value}\nvalue: {value_str}")
     else: return None
 
-print(var_check("x = '35434tg4r'", True))
+#print(var_check("x = 0b01", True))
 # Make sure there's nothing wrong with the syntax of a function
 def function_check(function: str) -> str:
-    if not isinstance(function, str): raise SyntaxError(f"type({function}) gave {type(function)} and not <class 'int'>")
+    if not isinstance(function, str): raise SyntaxError(f"type({function}) gave {type(function)} and not <class 'str'>")
 
     # Convert the string into a list
     L = [x for x in function]
-    bracket_start = False
-    bracket_end = False
 
     # Make sure the start begins with 'Func '
     for (i, v) in enumerate(L[:4]):
         if ["F", "u", "n", "c", " "][i] != v: raise SyntaxError(f"Function {function} doesn't begin with 'Func'")
-
     # Make sure the function has a name
-    for j in L[5:]:
-        if j == "[":
-            if bracket_start: raise SyntaxError(f"Function {function} has special characters in it's name")
-            bracket_start = True
-        elif j == "]":
-            if bracket_end: raise SyntaxError(f"Function {function} has special characters in it's name")
-        elif j in special_chars: raise SyntaxError(f"Function {function} has special characters in it's name")
-
-
+    for (index, value) in enumerate(L[5:]):
+        if value == " ":
+            if L[index+6] != "[": raise SyntaxError(f"Function {function} has a space in the name")
+            break
+        elif value in special_chars or value in ["=", "."]: raise SyntaxError(f"Function {function} has special characters in its name")
+    return "worked"
+print(function_check("Func testing []"))
